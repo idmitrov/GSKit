4bd4ac11613178ae108f5ef6cdef32ea4ae82318
dimitar.b.dimitrov <mailto.dimitrov@gmail.com>
2017-03-08 20:47:48 +0200

    Refactore config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
-var DS = '/',
-	ROOT = '.';
+const ROOT = '.';
 
 module.exports = {
 	path: {
-		root: DS + ROOT, 
+		root: `/${ROOT}`,
 		dev: {
-			index: ROOT + DS + 'src' + DS,
-			script: ROOT + DS + 'src' + DS + 'js' + DS,
-			style: ROOT + DS + 'src' + DS + 'scss' + DS
+			index: `${ROOT}/src/`,
+			script: `${ROOT}/src/js/`,
+			style: `${ROOT}/src/scss/`
 		},
 		prod: {
-			index: ROOT + DS + 'public' + DS,
-			script: ROOT + DS + 'public' + DS + 'js' + DS,
-			style: ROOT + DS + 'public' + DS + 'css' + DS,
-		}
+			index: `${ROOT}/dist/`,
+			script: `${ROOT}/dist/js/`,
+			style: `${ROOT}/dist/css/`
+		},
 	}
 };
